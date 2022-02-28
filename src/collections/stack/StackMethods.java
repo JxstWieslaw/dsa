71e60cd2fe42ff6ab2deae72a2be5db20fa92e7e
JxstWieslaw <wieslawsamushonga01@gmail.com>
2022-03-01 05:17:18 +0530

    StakMethods

--- src/collections/stack/StackMethods.java
+++ src/collections/stack/StackMethods.java
@@ -1,4 +1,23 @@
 package collections.stack;
 
+import java.util.Stack;
+
 public class StackMethods {
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push("A"); //add object to top
+        s.push("B");
+        s.push(1); //heterogeneous
+        System.out.println(s);
+        s.pop();//remove from top
+        System.out.println(s);
+        System.out.println(s.peek());//return top element but do not pop/delete
+        System.out.println("Stack after peek: "+s);
+        System.out.println(s.search("B"));
+        s.push("C");
+        s.push("D");
+        //after more elements are inserted on top
+        System.out.println("Result if object B is not found: "+s.search("B"));
+        System.out.println("Result if object Z is not found: "+ s.search("Z"));
+    }
 }
