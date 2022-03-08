cacdd5bfbf25306d12ce3428ef8b5f9741bcbcd3
JxstWieslaw <wieslawsamushonga01@gmail.com>
2022-03-08 12:43:25 +0530

    Uncommenting code for git push

--- src/heaps/HeapDecrease.java
+++ src/heaps/HeapDecrease.java
@@ -1,106 +1,96 @@
-package heaps;
+ package heaps;
 
-import java.util.Scanner;
+ import java.util.Scanner;
 
-public class HeapDecrease {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while(t-- >0){
-            int maxsize = sc.nextInt();
-            MinHeap minHeap = new MinHeap(maxsize);
-            for(int i=1;i<=maxsize;i++){
-                minHeap.insert(sc.nextInt());
-            }            
-        }
-        
-        
+ public class HeapDecrease {
+     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         int t = sc.nextInt();
+         while(t-- >0){
+             int maxsize = sc.nextInt();
+             int key = sc.nextInt();
+             MinHeap minHeap = new MinHeap(maxsize);
+             for(int i=1;i<=maxsize;i++){
+                 minHeap.insert(sc.nextInt());
+             }
+             minHeap.decreaseKey(key);
+             minHeap.printHeap();
+         }
+     }
+ }
 
-    }
-}
+ class MinHeap{
+     int maxsize;
+     int size;
+     int []heap;
 
-class MinHeap{
-   int maxsize;
-   int size;
-   int []heap;
-   
-   MinHeap(int maxsize){
-       this.maxsize = maxsize;
-       this.size = 0;
-       heap = new int [maxsize + 1];
-       heap[0] = Integer.MIN_VALUE;
-   }
+     MinHeap(int maxsize){
+         this.maxsize = maxsize + 1;
+         this.size = 0;
+         heap = new int [this.maxsize];
+         heap[0] = Integer.MIN_VALUE;
+     }
 
-   void insert(int element){
-       heap[++size] = element;
-   }
+     void insert(int element){
+         heap[++size] = element;
+         shiftUp(size);
+     }
 
-   void swap(){
+     void swap(int i,int parent){
+         int temp = heap[i];
+         heap[i] = heap[parent];
+         heap[parent] = temp;
+     }
 
-   }
+     void shiftUp(int index){ // minHeapify() going upwards;
+         int parent = index / 2;
+         if (index > 0 && heap[parent] > heap[index]) {
+             swap(index, parent);
+             shiftUp(parent);
+         }
+     }
 
-   void minHeapify(){
+     private void extractMin() {
+         heap[1] = heap[size];
+         size--;
+         shiftDown(heap, 1);
+     }
 
-   }
-   
-   void buildHeap(){
+     void decreaseKey(int key){
+         if (key < 1 || key >= heap.length - 1) {
+             return;
+         }
+         heap[key] = Integer.MIN_VALUE;
+         shiftUp(key);
+         extractMin();
+     }
 
-   }
+     void printHeap(){
+         for (int i = 1; i <=size; i++) {
+             System.out.print(heap[i]+ " ");
+         }
+         System.out.println();
+     }
 
-   void shiftDown(){
+     void shiftDown(int[] heap, int index) { //minHeapify(); going downwards.
 
-   }
+         int smallest = index;
 
-    void shiftUp(int index){
-        int parent = index / 2;
-        if (index > 0 && heap[parent] > heap[index]) {
-            int t = heap[index];
-            heap[index] = heap[parent];
-            heap[parent] = t;
-            shiftUp(parent);
-        }
-    }
-    void printHeap(){
-        for (int i = 1; i <=maxsize; i++) {
-            System.out.println(heap[i]+ " ");
-          }
-    }
+         int leftChild = 2 * index;
+         int rightChild = 2 * index + 1;
 
-    void decreaseKey(int key){
-        if (key < 1 || key >= heap.length - 1) {
-            return;
-        }
-        heap[key] = Integer.MIN_VALUE;
-        shiftUp(key);
-        extractMin();
-    }
+         if (leftChild <= size && heap[leftChild] < heap[smallest]) {
+             smallest = leftChild;
+         }
 
-    private void extractMin() {
-        heap[1] = heap[size];
-        size--;
-        shiftDown(heap, 1);
-    }
+         if (rightChild <= size && heap[rightChild] < heap[smallest]) {
+             smallest = rightChild;
+         }
 
-    private void shiftDown(int[] heap, int index) {
+         if (smallest != index) {
+             swap(smallest,index);
+             shiftDown(heap, smallest);
+         }
+     }
+ }
 
-        int smallest = index;
-        
-        int leftChild = 2 * index;
-        int rightChild = 2 * index + 1;
-        
-        if (leftChild < size && heap[leftChild] < heap[smallest]) {
-        smallest = leftChild;
-        }
-        
-        if (rightChild < size && heap[rightChild] < heap[smallest]) {
-        smallest = rightChild;
-        }
-        
-        if (smallest != index) {
-        int t = heap[index];
-        heap[index] = heap[smallest];
-        heap[smallest] = t;
-        shiftDown(heap, smallest);
-        }
-    }  
-}
